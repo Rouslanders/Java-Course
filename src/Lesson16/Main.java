9a7a0dfc9eb3c1b002864f4dd7eee929ffc7e07a
Руслан <rstyles@mail.ru>
2021-08-20 19:44:33 +0300

    реализован метод "общая стоимость" и "самый дорогой товар"

--- src/Lesson16/Main.java
+++ src/Lesson16/Main.java
@@ -7,18 +7,26 @@ public class Main {
         Tovar tovar1 = new Tovar(12312, "Монитор", 12000);
         Tovar tovar2 = new Tovar(12112, "Мышка  Logitech", 499);
         Tovar tovar3 = new Tovar(12313, "Моноблок Lenovo", 39000);
-        Tovar tovar4 = new Tovar(434, "Батарейка Lurasel", 100);
+        Tovar tovar4 = new Tovar(434, "Батарейка Durasel", 100);
         Tovar tovar5 = new Tovar(545, "Телефон Samsung", 23000);
 
-        sklad.addTovar(tovar1, 10);
-        sklad.addTovar(tovar1, 15);
-        sklad.addTovar(tovar2, 344);
-        sklad.addTovar(tovar3, 34);
-        sklad.addTovar(tovar4, 66);
-        sklad.addTovar(tovar5, 6);
+        sklad.addTovar(tovar1, 10); //10
+        sklad.addTovar(tovar1, 10); //15
+        sklad.addTovar(tovar2, 10); //344
+        sklad.addTovar(tovar3, 10); //34
+        sklad.addTovar(tovar4, 10); //66
+        sklad.addTovar(tovar5, 10); //6
 
-        sklad.deleteTovar(tovar1, 5);
-        System.out.println();
+      //  sklad.deleteTovar(tovar1, 5);
+        sklad.printAllTovars2();
+       // System.out.println(sklad.getMinCountTovar());
+
+        //вернуть самый дорогой товар
+        //посчитать общую стоимость всех товаров на складе
+        System.out.println(sklad.getMaxPriceTovar());
+        System.out.println(sklad.getAllPriceTovar());
+
+
     }
 
 
--- src/Lesson16/Sklad.java
+++ src/Lesson16/Sklad.java
@@ -1,6 +1,7 @@
 package Lesson16;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Sklad {
     private HashMap<Tovar, Integer> tovars = new HashMap<>();
@@ -15,13 +16,13 @@ public class Sklad {
 
     }
 
-    public  void deleteTovar(Tovar tovar, int value) {
-        if (!tovars.containsKey(tovar)){
+    public void deleteTovar(Tovar tovar, int value) {
+        if (!tovars.containsKey(tovar)) {
             System.out.println(" Такого товара на складе нет");
             return;
         }
 
-        if (value > tovars.get(tovar)){
+        if (value > tovars.get(tovar)) {
             System.out.println(" Товара на складе недостаточно ");
             tovars.remove(tovar);
             return;
@@ -29,6 +30,53 @@ public class Sklad {
 
         tovars.put(tovar, tovars.get(tovar) - value);
 
+    }
+
+    public void printAllTovars() {
+        for (Tovar t : tovars.keySet()) {
+            System.out.println(t.getName() + " - " + tovars.get(t));
+        }
+    }
+
+    //ниже аналогичный метод
+    public void printAllTovars2() {
+        for (Map.Entry<Tovar, Integer> para : tovars.entrySet()) {
+            System.out.println(para.getKey().getName() + " - " + para.getValue());
+        }
+    }
+
+    public Tovar getMinCountTovar() {
+        Tovar minTovar = null;
+        for (Tovar t : tovars.keySet()) {
+            if (minTovar == null) {
+                minTovar = t;
+            } else {
+                if (tovars.get(t) < tovars.get(minTovar)) {
+                    minTovar = t;
+                }
+            }
+        }
+        return minTovar;
+    }
+
+    public int getMaxPriceTovar() {
+        int maxPrice = 0;
+        for (Tovar t : tovars.keySet()){
+            if (t.getPrice() > maxPrice){
+                maxPrice = t.getPrice();
+
+            }
+        }
+        return maxPrice;
+    }
+
+    public int getAllPriceTovar (){
+        int priceAll = 0;
+        for (Tovar t : tovars.keySet()){
+        priceAll= priceAll + tovars.get(t)*t.getPrice();
+        }
+
+        return priceAll;
     }
 
 }
